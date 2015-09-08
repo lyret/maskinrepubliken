b717d296a66f9b95742e8418ca79b08d524de457
lyret <viktor@lyresten.se>
2015-09-08 21:54:18 +0200

    Fixes the bug where the gradient was removed from the hi-res background

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@
 /* Load a hi resoulution image in the background and add it to the body when done */
 $("#hi-res-background-image").off().on("load", function() {
     $("body").css({
-        "background-image" : "url(img/forest_hi_res.jpg)"
+        "background-image" : "linear-gradient( rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 1) ), url(img/forest_hi_res.jpg)"
     });
 });
